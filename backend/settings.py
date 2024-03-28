af77ae18546b0f3c82e02de7542b512e1eb61df7
type12-zz <joshuam36265@gmail.com>
2024-03-28 14:35:28 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'cosrheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
